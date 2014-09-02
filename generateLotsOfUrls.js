67fba4f66c86b8f0d3838a1f1fc5498e20efc485
divinity76 <divinity76@gmail.com>
2014-09-02 08:08:57 +0200

    it actually works now.. probably final commit

--- generateLotsOfUrls.js
+++ generateLotsOfUrls.js
@@ -1,36 +1,82 @@
-(function(){
+function generateMomondoTimeString(date) {
+    //return toLocaleDateString().replace(/\./ig,"-");//Date.getMonth() returns the month of the year -1?????? for crying out loud
+    return (("00" + date.getDate()).slice(-2)) + "-" + (("00" + (date.getMonth() + 1)).slice(-2)) + "-" + (("0000" + date.getFullYear()).slice(-4));
+    //just in case someone want to travel to Nazareth at year 0000 ? ;) will malfunction on dates earlier than 0000, 
+    // and right now i don't care enough to fix it
+};
 
-var buildGetUrl=function buildGetUrl($url,$getdata){
-var $ret=$url;
-//fixme,  $url="foo.com/?foo=bar";
-if($ret.split("?").length<2){
-$ret=$ret+"?";
-}
-if($ret.substr(-1)!="&" && $ret.substr(-1)!="?"){
-$ret=$ret+"&";
-}
-var $n,$i=0;
-for($n in $getdata){
-$ret=$ret+encodeURIComponent($n)+"="+encodeURIComponent($getdata[$n])+"&";
-++$i;
-}
-if($i>0){
-$ret=$ret.slice(0,-1);//remove the last & :p
-}
-return $ret;
-}
-var $url="http://www.momondo.no/multicity/";
+function generateMomondoTimeStrings(_fromDate, _toDate) {
+    var $ret = [],
+        fromDate = new Date(),
+        toDate = new Date();
+    fromDate.setTime(_fromDate.getTime());
+    toDate.setTime(_toDate.getTime());
+
+    while (fromDate.getTime() < (toDate.getTime() + 1)) {
+        $ret.push(generateMomondoTimeString(fromDate));
+        fromDate.setTime(fromDate.getTime() + (24 * 1000 * 60 * 60));
+    }
+    return $ret;
+};
+
+
+function buildGetUrl($url, $getdata) {
+    var $ret = $url;
+    //fixme,  $url="foo.com/?foo=bar";
+    if ($ret.split("?").length < 2) {
+        $ret = $ret + "?";
+    }
+    if ($ret.substr(-1) != "&" && $ret.substr(-1) != "?") {
+        $ret = $ret + "&";
+    }
+    var $n, $i = 0;
+    for ($n in $getdata) {
+        $ret = $ret + encodeURIComponent($n) + "=" + encodeURIComponent($getdata[$n]) + "&";
+        ++$i;
+    }
+    if ($i > 0) {
+        $ret = $ret.slice(0, -1); //remove the last & :p
+    }
+    return $ret;
+};
+
+function buildGetUrls($url, $getdata, $fromDate, $toDate) {
+    var $ret = [];
+    var originalSDP0 = $getdata["SDP0"]; //where is Erlang when i need it
+    var timeStrings = generateMomondoTimeStrings($fromDate, $toDate);
+    var i = 0,
+        tmp = "";
+    for (i = 0; i < timeStrings.length; ++i) {
+        $getdata["SDP0"] = timeStrings[i];
+        $ret.push(buildGetUrl($url, $getdata));
+    };
+    $getdata["SDP0"] = originalSDP0; //fuuuuu
+    return $ret;
+};
+var $url = "http://www.momondo.no/multicity/";
 //"http://www.momondo.no/multicity/?Search=true&TripType=oneway&SegNo=1&SO0=SGN&SD0=OSL&SDP0=10-09-2014&AD=1&TK=ECO&DO=false&NA=true#Search=true&TripType=oneway&SegNo=1&SO0=SGN&SD0=OSL&SDP0=10-09-2014&AD=1&TK=ECO&DO=false&NA=true";
- var $getdata={};
- $getdata["Search"]="true";
- $getdata["TripType"]="oneway";
- $getdata["SegNo"]="1";//<??? 
- $getdata["SO0"]="SGN";//<TRAVEL FROM CODE
- $getdata["SD0"]="OSL";//<TRAVEL TO, CODE
- $getdata["SDP0"]="10-09-2014";//<TRAVEL FROM DATE
- $getdata["AD"]="1";//<??? 
- $getdata["TK"]="ECO";//<??? (probably "ECO"=economy / business / firstclass, etc, but not confirmed...);
- $getdata["DO"]="false";//<???
- $getdata["NA"]="true";//<???
- return buildGetUrl($url,$getdata);
- })();
+var $getdata = {};
+$getdata["Search"] = "true";
+$getdata["TripType"] = "oneway";
+$getdata["SegNo"] = "1"; //?? 
+$getdata["SO0"] = "SGN"; //<TRAVEL FROM CODE
+$getdata["SD0"] = "OSL"; //<TRAVEL TO, CODE
+$getdata["SDP0"] = "10-09-2014"; //<TRAVEL FROM DATE
+$getdata["AD"] = "1"; //<??? 
+$getdata["TK"] = "ECO"; //<??? (probably "ECO"=economy / business / firstclass, etc, but not confirmed...);
+$getdata["DO"] = "false"; //<???
+$getdata["NA"] = "true"; //<???
+var urls = buildGetUrls($url, $getdata, (new Date("2014-09-03")), (new Date("2014-09-16")));
+document.body.innerHTML = "";
+var i = 0,
+    tmpe;
+for (i = 0; i < urls.length; ++i) {
+    tmpe = document.createElement("a");
+    tmpe.setAttribute("href", tmpe.href = urls[i]);
+    tmpe.setAttribute("target", tmpe.target = "_blank");
+    tmpe.textContent = tmpe.href;
+    document.body.appendChild(tmpe);
+    document.body.appendChild(document.createElement("br"));
+    document.body.appendChild(document.createElement("br"));
+    document.body.appendChild(document.createElement("br"));
+}
